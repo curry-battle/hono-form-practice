d90793cfcb010bd4a374d5762ceb771f6c8df0b9
curry-battle <omdwn.mo+github@gmail.com>
2025-05-02 23:19:11 +0900

    wip: add Form

---
+++ src/Form.tsx
@@ -0,0 +1,36 @@
+import type { Event } from "hono/jsx";
+
+export default function Form() {
+  // TODO: cannot hit this function
+  const handleSubmit = async (event: Event) => {
+    event.preventDefault();
+    console.log("Form submitted");
+
+    const target = event.currentTarget as HTMLFormElement;
+    const formData = new FormData(target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+
+    await fetch("/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        console.log("Success:", response);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    // React.FormEvent<HTMLFormElement> ではない!
+    <form onSubmit={(e: Event) => handleSubmit(e)}>
+      <input name="text" type="text" placeholder="Enter text" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
